0936a4269c1e5b37df987254ce73a5d08644ace1
JSY <jsy98515@gmail.com>
2023-06-12 18:00:04 +0000

    fix : bm25 embedding with tokenizer

--- code/retrieval.py
+++ code/retrieval.py
@@ -1078,52 +1078,33 @@ class BM25SparseRetrieval:
             with open(bm25_path, "rb") as file:
                 self.p_embedding = pickle.load(file)
             print("Embedding pickle load.")
-            print("passage embedding shape : {}".format(self.p_embedding.shape))
+            print("passage embedding shape : {}".format(self.p_embedding.corpus_size))
 
         else:
             print("Build passage embedding")
-
-            self.p_embedding = BM25Okapi(self.contexts)
+            # tokenized_corpus = [self.tokenize_fn(title + text) for title, text in zip(self.wiki_title, self.wiki_text)]
+            # self.bm25 = BM25Plus(tokenized_corpus)
+            self.p_embedding = BM25Okapi(self.contexts, tokenizer=self.tokenize_fn)
             with open(bm25_path, "wb") as file:
                 pickle.dump(self.p_embedding, file)
             print("BM25 pickle saved.")
 
-    def build_faiss(self, num_clusters=64) -> None:
+    # def build_faiss(self, num_clusters=64) -> None:
 
-        """
-        Summary:
-            속성으로 저장되어 있는 Passage Embedding을
-            Faiss indexer에 fitting 시켜놓습니다.
-            이렇게 저장된 indexer는 `get_relevant_doc`에서 유사도를 계산하는데 사용됩니다.
+    #     """
+    #     Summary:
+    #         속성으로 저장되어 있는 Passage Embedding을
+    #         Faiss indexer에 fitting 시켜놓습니다.
+    #         이렇게 저장된 indexer는 `get_relevant_doc`에서 유사도를 계산하는데 사용됩니다.
 
-        Note:
-            Faiss는 Build하는데 시간이 오래 걸리기 때문에,
-            매번 새롭게 build하는 것은 비효율적입니다.
-            그렇기 때문에 build된 index 파일을 저정하고 다음에 사용할 때 불러옵니다.
-            다만 이 index 파일은 용량이 1.4Gb+ 이기 때문에 여러 num_clusters로 시험해보고
-            제일 적절한 것을 제외하고 모두 삭제하는 것을 권장합니다.
-        """
-
-        indexer_name = f"faiss_clusters{num_clusters}.index"
-        indexer_path = os.path.join(self.data_path, indexer_name)
-        if os.path.isfile(indexer_path):
-            print("Load Saved Faiss Indexer.")
-            self.indexer = faiss.read_index(indexer_path)
-
-        else:
-            p_emb = self.p_embedding.astype(np.float32).toarray()
-            emb_dim = p_emb.shape[-1]
-
-            num_clusters = num_clusters
-            quantizer = faiss.IndexFlatL2(emb_dim)
-
-            self.indexer = faiss.IndexIVFScalarQuantizer(
-                quantizer, quantizer.d, num_clusters, faiss.METRIC_L2
-            )
-            self.indexer.train(p_emb)
-            self.indexer.add(p_emb)
-            faiss.write_index(self.indexer, indexer_path)
-            print("Faiss Indexer Saved.")
+    #     Note:
+    #         Faiss는 Build하는데 시간이 오래 걸리기 때문에,
+    #         매번 새롭게 build하는 것은 비효율적입니다.
+    #         그렇기 때문에 build된 index 파일을 저정하고 다음에 사용할 때 불러옵니다.
+    #         다만 이 index 파일은 용량이 1.4Gb+ 이기 때문에 여러 num_clusters로 시험해보고
+    #         제일 적절한 것을 제외하고 모두 삭제하는 것을 권장합니다.
+    #     """
+    #     pass
 
     def retrieve(
             self, 
@@ -1160,7 +1141,7 @@ class BM25SparseRetrieval:
                     "question": example["question"],
                     "id": example["id"],
                     # Retrieve한 Passage의 id, context를 반환합니다.
-                    "context_id": doc_indices[idx],
+                    # "context_id": doc_indices[idx],
                     "context": " ".join(
                         [self.contexts[pid] for pid in doc_indices[idx]]
                     ),
@@ -1199,20 +1180,20 @@ class BM25SparseRetrieval:
             doc_indices.append(sorted_result.tolist()[:k])
         return doc_scores, doc_indices
 
-    def retrieve_faiss(
-        self, query_or_dataset: Union[str, Dataset], topk: Optional[int] = 1
-    ) -> Union[Tuple[List, List], pd.DataFrame]:
-        pass
+    # def retrieve_faiss(
+    #     self, query_or_dataset: Union[str, Dataset], topk: Optional[int] = 1
+    # ) -> Union[Tuple[List, List], pd.DataFrame]:
+    #     pass
 
-    def get_relevant_doc_faiss(
-        self, query: str, k: Optional[int] = 1
-    ) -> Tuple[List, List]:
-        pass
+    # def get_relevant_doc_faiss(
+    #     self, query: str, k: Optional[int] = 1
+    # ) -> Tuple[List, List]:
+    #     pass
 
-    def get_relevant_doc_bulk_faiss(
-        self, queries: List, k: Optional[int] = 1
-    ) -> Tuple[List, List]:
-        pass
+    # def get_relevant_doc_bulk_faiss(
+    #     self, queries: List, k: Optional[int] = 1
+    # ) -> Tuple[List, List]:
+    #     pass
 
 
 def get_args():
@@ -1234,7 +1215,6 @@ def get_args():
     )
     parser.add_argument("--method", default='tfidf', type=str, help="")
     parser.add_argument("--topk", default=10, type=int, help="")
-    parser.add_argument("--ngram", default=2, type=int, help="")
 
     args = parser.parse_args()
     return args
