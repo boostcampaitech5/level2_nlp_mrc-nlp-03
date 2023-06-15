cdf5c369a05545656e56286d761ef570c55b9259
JSY <jsy98515@gmail.com>
2023-06-15 20:35:49 +0000

    fix : train/inference error

--- code/inference.py
+++ code/inference.py
@@ -152,6 +152,7 @@ def run_retrieval(
                 "question": Value(dtype="string", id=None),
             }
         )
+    df = df[list(f)]
     datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
     return datasets
 
